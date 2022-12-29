a3cf95cf4dd2efc3f4aedc893196b515f7818180
minhnhatnguyenn <96915421+minhnhatnguyenn@users.noreply.github.com>
2022-12-29 14:34:11 +0700

    Creater footer
    date modified: 2h33 29/12

---
+++ layout/Footer.php
@@ -0,0 +1,100 @@
+<!-- FOOTER -->
+<footer id = "footer">
+    <!-- top footer -->
+    <div class = "section">
+        <!-- container -->
+        <div class = "container">
+            <!-- row -->
+            <div class = "row">
+                <div class = "col-md-3 col-xs-6">
+                    <div class = "footer">
+                        <h3 class = "footer-title">Về chúng tôi</h3>
+                        <ul class = "footer-links">
+                            <li>
+                                <a href = "#"><i class = "fa fa-map-marker"></i>12 Chùa Bộc, Hà Nội</a>
+                            </li>
+                            <li>
+                                <a href = "#"><i class = "fa fa-phone"></i>0868197116</a>
+                            </li>
+                            <li>
+                                <a href = "#"><i class = "fa fa-envelope-o"></i>Chikara@email.com</a>
+                            </li>
+                            <li>
+                                <a><iframe src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.5884918509983!2d105.82661915060876!3d21.009126385940355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac800f450807%3A0x419a49bcd94b693a!2zSOG7jWMgdmnhu4duIE5nw6JuIGjDoG5n!5e0!3m2!1svi!2s!4v1640889844491!5m2!1svi!2s" width = "815" height = "250" style = "border:0;" allowfullscreen = "" loading = "lazy"></iframe></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class = "col-md-3 col-xs-6">
+                    <div class = "footer">
+                        <h3 class = "footer-title">Theo dõi chúng tôi</h3>
+                        <ul class = "footer-links">
+                            <li>
+                                <a href = "https://www.facebook.com/Chikara-Electric-104182552149002" target="_blank"><i class = "fa fa-facebook-square"></i>Chikara</a>
+                            </li>
+                            <li>
+                                <a href = "http://mis.hvnh.edu.vn/" target="_blank"><i class = "fa fa-youtube-play"></i>Chikara Office</a>
+                            </li>
+                            <li>
+                                <a href = "http://mis.hvnh.edu.vn/" target="_blank"><i class = "fa fa-instagram"></i>Chikara</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class = "clearfix visible-xs"></div>
+
+                <div class = "col-md-3 col-xs-6">
+                    <div class = "footer">
+                        <h3 class = "footer-title">Chính sách</h3>
+                        <ul class = "footer-links">
+                            <li>
+                                <a href="#">Chính sách chung</a>
+                            </li>
+                            <li>
+                                <a href="#">Chính sách bảo hành</a>
+                            </li>
+                            <li>
+                                <a href="#">Chính sách vận chuyển</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class = "col-md-3 col-xs-6">
+                    <div class = "footer">
+                        <h3 class = "footer-title">Dịch vụ</h3>
+                        <ul class = "footer-links">
+                            <li>
+                                <a href = "#">Tài khoản</a>
+                            </li>
+                            <li>
+                                <a href = "#">Giúp đỡ</a>
+                            </li>
+                            <li>
+                                <a href = "#">Góp ý</a>
+                            </li>
+                            <li>
+                                <img src = "img/Baocongthuong.png" alt = "Baocongthuong" style = "width: 100px; height: 50px">
+                            </li>
+                            <li>
+                                <img src = "img/visa.png" alt = "thanhtoan" style = "width: 100px; height: 50px">
+                            </li>
+                            <li>
+                                <img src = "img/homecredit.png" alt = "homecredit" style = "width: 100px; height: 50px">
+                            </li>
+                            <li>
+                                <img src = "img/paypal.png" alt = "paypal" style = "width: 100px; height: 50px">
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- /row -->
+        </div>
+        <!-- /container -->
+    </div>
+    <!-- /top footer -->
+</footer>
+<!-- /FOOTER -->
