c6af41dadb7b68389ce3807039c35b2c262bb7cc
minhnhatnguyenn <96915421+minhnhatnguyenn@users.noreply.github.com>
2023-01-05 16:02:30 +0700

    update home

--- layout/home.php
+++ layout/home.php
@@ -93,11 +93,7 @@
 												?>
 											</del>
 										</h4>
-										<div class="product-btns">
-											<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
-											<button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
-											<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
-										</div>
+										
 									</div>
 									<div class="add-to-cart">
 										<button class="add-to-cart-btn" id="<?php echo $rows['id']; ?>">add to cart</button>
@@ -204,11 +200,7 @@
 											}
 										?>
 										</h4>
-										<div class="product-btns">
-											<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
-											<button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
-											<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
-										</div>
+										
 									</div>
 									<div class="add-to-cart">
 										<button class="add-to-cart-btn" id="<?php echo $rows['id']; ?>">add to cart</button>
@@ -312,11 +304,7 @@
 												<?php
 													}
 												?>
-												<div class="product-btns">
-													<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
-													<button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
-													<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
-												</div>
+												
 											</div>
 											<div class="add-to-cart">
 												<button class="add-to-cart-btn" id="<?php echo $rows['id']; ?>">add to cart</button>
@@ -394,11 +382,7 @@
 														?>
 													</del>
 												</h4>
-												<div class="product-btns">
-													<button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span class="tooltipp">add to wishlist</span></button>
-													<button class="add-to-compare"><i class="fa fa-exchange"></i><span class="tooltipp">add to compare</span></button>
-													<button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">quick view</span></button>
-												</div>
+											
 											</div>
 											<div class="add-to-cart">
 											<button class="add-to-cart-btn" id="<?php echo $rows['id']; ?>">add to cart</button>
